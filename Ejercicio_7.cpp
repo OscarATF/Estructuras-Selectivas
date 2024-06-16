3852d6569add8a6ec417ae55e1e17698034b7d5f
Oscar <oticonaf@unjbg.edu.pe>
2024-06-16 15:04:25 -0500

    Agrega Ejercicio_7 de una tienda de llantas, Estructuras-Selectivas

---
+++ Ejercicio_7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double numLl,precioU,totalP;
+	cout<<"En una llantera se ha establecido una promocion de las llantas marca Ponchadas, dicha promocion consiste en lo siguiente:\nSi se compran menos de cinco llantas el precio es de $300 cada una,\nde $250 si se  compran de cinco a 10 y de $200 si se compran mas de 10."<<endl;
+	cout<<"Programa para obtener la cantidad de dinero que una persona tiene que pagar por cada una de las llantas que compra y la que tiene que pagar por el total de la compra."<<endl;
+	cout<<"*Ingrese la cantidad de llantas que comprara:"<<endl;
+	cin>>numLl;
+	if (numLl<5) {
+		precioU=300;
+	} else if (numLl<=10) {
+		precioU=250;		
+	} else {
+		precioU=200;
+	}
+	totalP=precioU*numLl;
+	cout<<"Por cada llanta debe pagar s/"<<precioU<<" y en total s/"<<totalP<<endl;
+	return 0;
+}
